4cdba7858e07af8dc4a3f30d2aeb8e7ea5f25540
Zack Porter <jeresnine@gmail.com>
2014-05-27 12:24:51 -0600

    Added stuff in validate user

--- src/main/java/indexer/server/ValidateUserHandler.java
+++ src/main/java/indexer/server/ValidateUserHandler.java
@@ -3,12 +3,36 @@ package main.java.indexer.server;
 import java.io.IOException;
 
 import com.sun.net.httpserver.*;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import main.java.indexer.server.daos.Database;
 
 
 public class ValidateUserHandler implements HttpHandler{
 
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
+
+		Database database = new Database();
+		XStream xmlStream = new XStream(new DomDriver());
+		ValidateUserParam param = xmlStream.fromXML(exchange.getRequestBody());
+
+		try {
+			db.startTransaction();
+			db.get
+			db.endTransaction();
+		}
+		catch (ServerException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			db.endTransaction(false);
+			exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, -1);
+			return;
+		}
+
+		exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, -1);
+
 		
 		// Process AddContact request
 		// 1. Deserialize the request object from the request body
