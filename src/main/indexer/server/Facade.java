2e6eca7b50b40368b964b9a961530c142b6b1185
Zachary Porter <jeresnine@gmail.com>
2014-06-02 17:46:24 -0600

    Bug Fix

--- src/main/indexer/server/Facade.java
+++ src/main/indexer/server/Facade.java
@@ -221,14 +221,16 @@ public class Facade{
 	 */
 	public GetFields_Result getFields(String auth, int projectId){
 		GetFields_Result result = new GetFields_Result();
-		if(projectId == 0){
+		Database database = new Database();
+		database.startTransaction();
+		Project project = database.getProjectDAO().readProject(projectId);
+		if(projectId == 0 || project == null){
+			database.endTransaction();
 			result.setError(true);
 			return result;
 		}
-		Database database = new Database();
 		List<Field> fields = new ArrayList<>();
 		if(validateUser(auth).isValid()){
-			database.startTransaction();
 			fields = database.getFieldDAO().readFieldsForProject(projectId);
 			database.endTransaction();
 			if(database.wasSuccesful()){
